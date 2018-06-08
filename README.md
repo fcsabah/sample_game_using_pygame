1654ca5907390eebb8688298644e49f50f5bfe96
fcsabah <fahadbd1993@gmail.com>
2018-06-08 16:53:44 +0600

    this is the first commit upto 7 episode.

---
+++ README.md
@@ -0,0 +1,5 @@
+This is a learning and testing repository. This covers the sentdex Game Development in Python 3 With PyGame:
+
+https://www.youtube.com/playlist?list=PLQVvvaa0QuDdLkP8MrOXLe_rKuf6r80KO
+
+Just some random noobie games!!!
---
+++ test.py
@@ -0,0 +1,101 @@
+import pygame
+import time
+import random
+
+pygame.init()
+
+ScreenHeight = 800
+ScreenWidth = 600
+
+gameDisplay = pygame.display.set_mode((ScreenHeight, ScreenWidth))
+pygame.display.set_caption("surprise :D")
+
+black = (0,0,0)
+white = (255,255,255)
+
+clock = pygame.time.Clock()
+
+carImg = pygame.image.load("racecar.png")
+car_width = 100
+#print(carImg)
+
+def Things(thingX, thingY, thingH, thingW, color):
+	pygame.draw.rect(gameDisplay, color, [thingX, thingY, thingW, thingH])
+
+def Car(x,y):
+	gameDisplay.blit(carImg, (x,y))
+
+def crash():
+    message_display('You Crashed')
+
+def message_display(text):
+    largeText = pygame.font.Font('freesansbold.ttf',100)
+    TextSurf, TextRect = text_objects(text, largeText)
+    TextRect.center = ((ScreenWidth/1.5),(ScreenHeight/3))
+    gameDisplay.blit(TextSurf, TextRect)
+    pygame.display.update()
+    time.sleep(2)
+    game_loop()
+
+def text_objects(text, font):
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
+
+def game_loop():
+	x = (ScreenWidth * 0.45)
+	y = (ScreenHeight * 0.6)
+	xChange = 0
+	carSpeed = 0
+	gameExit = False
+
+	thing_startx = random.randrange(0, ScreenWidth)
+	thing_starty = -600
+	thing_speed = 5
+	thing_width = 100
+	thing_height = 100
+
+	while not gameExit:
+		for event in pygame.event.get():
+			#print(event)
+			if event.type == pygame.QUIT:
+				print "Game exited by user"
+				pygame.quit()
+				quit()
+
+
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_LEFT:
+					xChange = -5
+				elif event.key == pygame.K_RIGHT:
+					xChange = 5
+
+
+			if event.type == pygame.KEYUP:
+				if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+					xChange = 0
+
+		x += xChange
+		print(x)
+
+		gameDisplay.fill(white)
+		Things(thing_startx, thing_starty, thing_width, thing_height, black)
+		thing_starty += thing_speed
+		Car(x,y)
+
+		if x > ScreenWidth + car_width or x < 0:
+			crash()
+
+		if thing_starty > ScreenHeight:
+			thing_starty = 0 - thing_height
+			thing_startx = random.randrange(0,ScreenWidth)
+
+		if y < thing_starty + thing_height:
+			if x > thing_startx and x < thing_startx + thing_width or x+car_width > thing_startx and x + car_width < thing_startx+thing_width:
+				crash()
+
+		pygame.display.flip()
+		clock.tick(60)
+
+game_loop()
+
+pygame.quit()
